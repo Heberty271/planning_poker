3e93f09522d3e2bbd7877b3531e9d3a21b73d74b
Eliezer Cardoso Alves <15219317636@MUZAMBINHO.EDU.BR>
2022-04-11 22:14:56 -0300

    refact(room): limpando pagina da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -8,58 +8,25 @@ type Room = {
 
 export default function Room() {
   const [name, setName] = useState('');
-  const [rooms, setRooms] = useState<Room[]>([
-    {
-      name: 'Room 1',
-      createdAt: 'hoje'
-    },
-    {
-      name: 'Room 2',
-      createdAt: 'hoje'
-    },
-    {
-      name: 'Room 3',
-      createdAt: 'hoje'
-    }
-  ]);
-
-  useEffect(() => {    
-    database.ref('rooms').on('value', (_rooms) => {
-      const firebaseRooms = _rooms.val();
+  const [rooms, setRooms] = useState<Room[]>([])
 
 
-      Object.entries(firebaseRooms).map(([key, value]:any) => {
-        console.log(key, value);
-        const sala = {
-          name: value.name,
-          createdAt: value.createdAt
-        }
-
-        setRooms([...rooms, sala])        
-        
-      })
-    });
-
-    return () => {}
-  })
-
-  async function criarSala(e:any) {
+  async function handleSubmit(e:any) {
     e.preventDefault();
     var data = new Date();
+    const createdAt = `${String(data.getDate()).padStart(2, '0')}-${String(data.getMonth() + 1).padStart(2, '0')}-${data.getFullYear()}`;
+
     const newRoom = {
       name: name,
-      createdAt: `${String(data.getDate()).padStart(2, '0')}-${String(data.getMonth() + 1).padStart(2, '0')}-${data.getFullYear()}`,
+      createdAt: createdAt
     }
-
-    database.ref('rooms').push(newRoom)
-    setName('')
     
   }
 
   return (
     <>
       <h1>Página de Salas</h1>
-      <form onSubmit={(e) => criarSala(e)}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <fieldset>
           <legend>Criar nova sala</legend>
           <label htmlFor="name">Digite o nome da sala</label>
