98307b1debeda8fd57a1eb7f6373a7a579e6f10c
Eliezer Cardoso Alves <15219317636@MUZAMBINHO.EDU.BR>
2022-04-25 20:08:08 -0300

    feat: funcionalidade de criar sala e resgatar os dados

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useRoom } from "../hooks/useRoom";
 import { database } from '../services/firebase';
 
 type Room = {
@@ -6,58 +8,26 @@ type Room = {
   createdAt: string;
 }
 
+type RoomParams = {
+  id: string
+}
+
 export default function Room() {
-  const [name, setName] = useState('');
-  const [rooms, setRooms] = useState<Room[]>([
-    {
-      name: 'Room 1',
-      createdAt: 'hoje'
-    },
-    {
-      name: 'Room 2',
-      createdAt: 'hoje'
-    },
-    {
-      name: 'Room 3',
-      createdAt: 'hoje'
-    }
-  ]);
+  const { name, roomCode } = useRoom()
   
-
-  async function handleSubmit(e:any) {
-
-    e.preventDefault();
-    var data = new Date();
-    const createdAt = `${String(data.getDate()).padStart(2, '0')}-${String(data.getMonth() + 1).padStart(2, '0')}-${data.getFullYear()}`;
-
-    const newRoom = {
-      name: name,
-      createdAt: createdAt
-    }
-    
+  if (!roomCode) {
+    return (
+      <>
+        <h1>Sala não encontrada</h1>
+      </>
+    )
   }
+
 
   return (
     <>
-      <h1>Página de Salas</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <fieldset>
-          <legend>Criar nova sala</legend>
-          <label htmlFor="name">Digite o nome da sala</label>
-          <input onChange={(e) => {setName(e.target.value); }} type="text" id="name" value={name} />          
-          <button type="submit">salvar</button>
-        </fieldset>
-      </form>
-      <h2>Salas Cadastradas</h2>
-      <ul>
-        {
-          rooms.map((room) => {
-            return (
-              <li>{room.name}</li>
-            )
-          })
-        }
-      </ul>
+      <h1>{name} - {roomCode} </h1>
+
     </>
   )
 }
