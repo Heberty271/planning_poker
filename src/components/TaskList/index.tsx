bbc8b8dd9a6f6baf24ac0be712de5188e8a532b5
Eliezer Cardoso Alves <15219317636@MUZAMBINHO.EDU.BR>
2022-05-23 22:32:35 -0300

    feat: listando e excluindo tarefas em uma sala

---
+++ src/components/TaskList/index.tsx
@@ -0,0 +1,30 @@
+import { useRoom } from "../../hooks/useRoom"
+import binIcon from './images/bin-icon.svg'
+import cx from 'classnames';
+
+export function TaskList() {
+  const { tasks, deleteTask } = useRoom()
+
+  return (
+    <div className="w-full flex-col-center gap-4">
+      {
+        tasks.map(task =>{
+          return (
+            <div key={task.id}
+              className={cx('w-full p-4 pt-2 flex flex-col bg-gray-100 rounded-lg shadow-md',
+                {'border-2 border-primary-200 bg-primary-50': false}
+              )}>
+              <div onClick={() => deleteTask(task.id)} className="w-full flex items-center justify-end">
+                <img src={binIcon} width="42" alt="" className="p-2 link rounded-full hover:bg-gray-200" />
+              </div>              
+              <h3 className="mb-6">{task.title}</h3>
+              <div className="w-full flex items-center justify-between">                
+                <span className="btn-sm btn-secondary text-sm font-sans">{task.average}</span>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
